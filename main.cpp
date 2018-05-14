a50ad10a1e68c7ed29342d2cc54a44361eab2ee3
grzesiu <grzspychala@gmail.com>
2018-05-14 15:23:43 +0200

    Generate random numbers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
+#include <random>
 #include <iostream>
+#include <utility>
+#include <functional>
+#include <algorithm>
+
+
+std::vector<std::pair<double, double>> draw_lines(unsigned long n, double min, double max) {
+
+    std::vector<std::pair<double, double>> lines(n);
+
+    std::random_device rnd_device;
+    std::mt19937 mersenne_engine(rnd_device());
+    std::uniform_real_distribution<double> dist(min, max);
+
+    auto gen = std::bind(dist, mersenne_engine);
+    std::vector<int> vec(10);
+    std::generate(std::begin(vec), std::end(vec), gen);
+
+    for (auto i : vec) {
+        std::cout << i << " ";
+    }
+
+    return lines;
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    draw_lines(10, 0, 10);
     return 0;
 }
