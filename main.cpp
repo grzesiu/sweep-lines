dd4ce31972a42dee64eeac3ff657b68b8a211c3c
grzesiu <grzspychala@gmail.com>
2018-05-14 18:46:25 +0200

    Create Endpoint struct

--- main.cpp
+++ main.cpp
@@ -5,42 +5,67 @@
 #include <algorithm>
 
 template<class RealType = double>
-class uniform_pair_distribution {
-
+class SweepLines {
 public:
-    uniform_pair_distribution(RealType a, RealType b) {
-        dist = std::uniform_real_distribution<RealType>(a, b);
+
+    struct Endpoint {
+        RealType x;
+        RealType y;
+        unsigned long line_id;
+
+        Endpoint() {};
+
+        Endpoint(RealType x, RealType y, unsigned long line_id) :
+                x(x), y(y), line_id(line_id) {}
+    };
+
+
+    static std::vector <Endpoint> draw_lines(unsigned long n, RealType min, RealType max) {
+
+        std::vector <Endpoint> endpoints(n * 2);
+
+        std::random_device rnd_device;
+        std::mt19937 mersenne_engine(rnd_device());
+        uniform_endpoint_distribution dist(min, max);
+
+        auto gen = std::bind(dist, mersenne_engine);
+        std::generate(std::begin(endpoints), std::end(endpoints), gen);
+
+        return endpoints;
     }
 
-    template<class Generator>
-    std::pair<RealType, RealType> operator()(Generator &g) {
-        return std::make_pair(dist(g), dist(g));
+
+    static void print(std::vector <Endpoint> lines) {
+        for (auto i : lines) {
+            std::cout << i.x << " " << i.y << " " << i.line_id << std::endl;
+        }
     }
+
+
 
 private:
-    std::uniform_real_distribution<RealType> dist;
+    class uniform_endpoint_distribution {
+
+    public:
+        uniform_endpoint_distribution(RealType a, RealType b) {
+            dist = std::uniform_real_distribution<RealType>(a, b);
+        }
+
+        template<class Generator>
+        Endpoint operator()(Generator &g) {
+            return Endpoint(dist(g), dist(g), count++ % 2);
+        }
+
+    private:
+        unsigned long count = 0;
+        std::uniform_real_distribution <RealType> dist;
+    };
+
 
 };
 
-std::vector<std::pair<double, double>> draw_lines(unsigned long n, double min, double max) {
-
-    std::vector<std::pair<double, double>> lines(n);
-
-    std::random_device rnd_device;
-    std::mt19937 mersenne_engine(rnd_device());
-    uniform_pair_distribution<double> dist(min, max);
-
-    auto gen = std::bind(dist, mersenne_engine);
-    std::generate(std::begin(lines), std::end(lines), gen);
-
-    return lines;
-}
-
 int main() {
-    std::vector<std::pair<double, double>> lines = draw_lines(10, 0, 10);
-    for (auto i : lines) {
-        std::cout << i.first << " " << i.second << std::endl;
-    }
-
+    std::vector <SweepLines<double>::Endpoint> lines = SweepLines<double>::draw_lines(10, 0, 10);
+    SweepLines<double>::print(lines);
     return 0;
 }
