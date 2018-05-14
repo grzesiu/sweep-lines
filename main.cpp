c13479803032c9ed614a0ffca3e7c51a66b514f3
grzesiu <grzspychala@gmail.com>
2018-05-14 15:46:31 +0200

    Generate random vector of pairs

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@
 #include <functional>
 #include <algorithm>
 
+template<class RealType = double>
+class uniform_pair_distribution {
+
+public:
+    uniform_pair_distribution(RealType a, RealType b) {
+        dist = std::uniform_real_distribution<RealType>(a, b);
+    }
+
+    template<class Generator>
+    std::pair<RealType, RealType> operator()(Generator &g) {
+        return std::make_pair(dist(g), dist(g));
+    }
+
+private:
+    std::uniform_real_distribution<RealType> dist;
+
+};
 
 std::vector<std::pair<double, double>> draw_lines(unsigned long n, double min, double max) {
 
@@ -11,21 +28,19 @@ std::vector<std::pair<double, double>> draw_lines(unsigned long n, double min, d
 
     std::random_device rnd_device;
     std::mt19937 mersenne_engine(rnd_device());
-    std::uniform_real_distribution<double> dist(min, max);
+    uniform_pair_distribution<double> dist(min, max);
 
     auto gen = std::bind(dist, mersenne_engine);
-    std::vector<int> vec(10);
-    std::generate(std::begin(vec), std::end(vec), gen);
-
-    for (auto i : vec) {
-        std::cout << i << " ";
-    }
+    std::generate(std::begin(lines), std::end(lines), gen);
 
     return lines;
-
 }
 
 int main() {
-    draw_lines(10, 0, 10);
+    std::vector<std::pair<double, double>> lines = draw_lines(10, 0, 10);
+    for (auto i : lines) {
+        std::cout << i.first << " " << i.second << std::endl;
+    }
+
     return 0;
 }
